94d205a90cdb7bf19fd82b9c8d1ed7e87e09736c
Vladimir Smykovsky <vladimireur@gmail.com>
2025-07-03 16:12:19 +0300

    Закреплен порт фронтенда на 5173

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Указываем желаемый порт
+    // host: '0.0.0.0', // Можно также указать хост, если нужно слушать на всех интерфейсах
+  },
 })
+
+
+
